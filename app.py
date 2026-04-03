32efa2b2ada8cbc4ea870c9f0746ad75c4f4e3ed
= <lmonferrari@outlook.com>
2026-04-03 18:47:32 -0300

    novo

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 dt = pytz.timezone('America/New_York').localize(datetime(2022, 1, 1)) 
 
+continue 
 print(3 != 1)
 
 class Teste:
@@ -12,6 +13,3 @@ class Teste:
     
     def teste2(self):
         return 2
-    
-break
-continue
