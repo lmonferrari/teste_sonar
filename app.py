9dd8ca4228f50515199c97d44f04212f5dbea8ac
= <lmonferrari@outlook.com>
2026-04-03 22:30:59 -0300

    a

--- app.py
+++ app.py
@@ -17,8 +17,13 @@ print(Teste.teste())
 print(Teste().teste2())
 
 print(dt)
-print(dt.astimezone(pytz.timezone('America/Sao_Paulo')))
+
 print(1 == 1)
+print(2 == 2)
+print(3 == 3)
+print(4 == 4)
 
 print(1 != 1)
-print(1 > 1)
+print(2 != 2)
+print(3 != 3)
+print(4 != 4)
